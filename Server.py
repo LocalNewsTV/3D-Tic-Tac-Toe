a42382dca0950e67988102616439e515b2e14aff
MLogan <loganmatthewr@gmail.com>
2022-09-17 22:16:12 -0700

    Documented and magic numbers removed

--- Server.py
+++ Server.py
@@ -6,13 +6,27 @@ BUF_SIZE = 1024
 HOST = ''
 PORT = 12345
 
+COMMAND = 0
+LAYER = 1
+ROW = 2
+COLUMN = 3
+TOKEN = 4
+TOKEN_START = 1
+TOKEN_MAX = 3
+MATCH_ONCE_ONLY = 1
+
 ###########################################################################
-# @returns {Array} Array representing a 4x4x4 game board 
+# @desc - Sets up a blank gameboard to play with
+# @returns {Array} - Array representing a 4x4x4 game board 
 ###########################################################################
 def setUpBoard():
-    return [[['_' for _ in range(4)]for _ in range(4) ] for _ in range(4) ] 
+    GRID_SIZE = 4
+    return [[['_' for _ in range(GRID_SIZE)]for _ in range(GRID_SIZE) ] for _ in range(GRID_SIZE) ] 
+
 ###########################################################################
-# @returns {string} easily readable stringified version of our game board array
+# @description - Stringifies the gameBoard to be returned by the TCP server
+# @param gameBoard {Array} - Current gameboard in play
+# @returns {String} - Easily readable stringified version of our gameBoard array
 ###########################################################################
 def displayBoard(gameBoard):
     boardString = ''
@@ -25,39 +39,64 @@ def displayBoard(gameBoard):
     boardString += '\n'
     return boardString
 
-def makeTurn(num, gameBoard):
-    gameBoard[int(num[1])][int(num[2])][int(num[3])] = int(num[4])
+###########################################################################
+# @desc - Compares users token to the current Token. if valid, makes move.
+# @param requestArray {Array} - Users move input, put into an array
+# @param gameBoard {Array} - current gameBoard in use
+# @param currentToken {integer} - valid turn token for the game
+# @returns {String} - [E]rror or [O]K for TCP servers response
+###########################################################################
+def usersMove(requestArray, gameBoard, currentToken):
+    if (requestArray[TOKEN] == currentToken):
+        if makePlayerMove(requestArray, gameBoard):
+            return 'O\n'
+    return "E\n"
 
-def validateTurn(num, gameBoard):
-    print(num)
-    if gameBoard[int(num[1])][int(num[2])][int(num[3])] == '_':
-        makeTurn(num, gameBoard)
+###########################################################################
+# @desc - Checks the space selected by user for "_", then places Token
+# @param rA {Array} - Input from user. "requestArray" shortened
+# @param gameBoard {Array} - current gameBoard in use
+# @returns {boolean} - turn successful
+###########################################################################
+def makePlayerMove(rA, gameBoard):
+    if gameBoard[rA[LAYER]][rA[ROW]][rA[COLUMN]] == '_':
+        gameBoard[rA[LAYER]][rA[ROW]][rA[COLUMN]] = rA[TOKEN]
         return True
     else:
         return False
-def usersMove(userRequest, gameBoard, currentToken):
-    validToken = (int(userRequest[4]) == currentToken)
-    if validToken:
-        validTurn = validateTurn(userRequest, gameBoard)
-        if validTurn:
-            return 'O\n'
-        return "E\n" #delete later
-    return "E\n"
+###########################################################################
+# @desc - Checks current value of token and resets if necessary
+# @param currentToken {integer} - current value of token when turn was made
+# @returns {integer} - updated Token value
+###########################################################################
 def updateToken(currentToken):
     currentToken += 1
-    if currentToken > 3:
-        currentToken = 1
+    if currentToken > TOKEN_MAX:
+        currentToken = TOKEN_START
     return currentToken
+
+###########################################################################
+# @desc - Main controller for the TCP server game. constantly listens for an input
+#   and acts accordingly. sends an encoded string as a response and 
+#   severs the connection. runs indefinitely 
+#   User inputs should be: 
+#       "G" - GET
+#       "P" - PUT
+#       "C" - CLEAR
+#   Server should respond with:
+#        A Stringified Board
+#       "O" - OK
+#       "E" - ERROR
+###########################################################################
 def boardGame():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: # TCP socket
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Details later
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((HOST, PORT)) # Claim messages sent to port "PORT"
         sock.listen(1) # Enable server to receive 1 connection at a time
         print('Server:', sock.getsockname()) # Source IP and port
         gameBoard = setUpBoard()
         serverResponse = ''
-        currentToken = 1
-        
+        currentToken = TOKEN_START
         while True:
             sc, sockname = sock.accept() # Wait until a connection is established
             
@@ -65,22 +104,22 @@ def boardGame():
                 print('Client:', sc.getpeername()) # Dest. IP and port
                 data = sc.recv(BUF_SIZE) # recvfrom not needed since address is known
                 userRequest = data.decode().strip().upper()
-                userRequestMove = len(re.findall('^P[0-3][0-3][0-3][0-3]$', userRequest)) == 1
+                userRequestMove = len(re.findall('^P[0-3][0-3][0-3][0-3]$', userRequest)) == MATCH_ONCE_ONLY
                 
                 if(userRequest == 'G'):
                     serverResponse = displayBoard(gameBoard)
                 elif userRequestMove:
-                    serverResponse = usersMove(userRequest, gameBoard, currentToken)
-                    if serverResponse[0] == 'O':
+                    requestArray = [int(userRequest[LAYER]), int(userRequest[ROW]), int(userRequest[COLUMN]), int(userRequest[TOKEN])]
+                    serverResponse = usersMove(requestArray, gameBoard, currentToken)
+                    if serverResponse[COMMAND] == 'O':
                         currentToken = updateToken(currentToken)
                 elif userRequest == 'C':
                     gameBoard = setUpBoard()
-                    currentToken = 1
+                    currentToken = TOKEN_START
                     serverResponse = 'O\n'
-                elif userRequest == 'CT':
-                    serverResponse = str(currentToken) + '\n'
                 else:
                     serverResponse = 'E\n'
+                    
                 sc.sendall(serverResponse.encode('utf-8')) # Dest. IP and port implicit due to accept call
 
 boardGame()
